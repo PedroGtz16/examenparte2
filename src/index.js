4eb1ebe96b112d4bd5cc23298b8d3370b373a67c
PedroGtz <pedr0.gtz@hotmail.com>
2021-03-17 21:23:48 -0600

    pos otro commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
-exports.renderIndex = (req, res) => {
-    res.render("index");
-}
-
-exports.postData = (req, res) =>{
-    console.log(req.body)
-    res.render ("index", {
-        delpost:req.body.fname
-    });
-}
-//--------------------
-
 const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
 const { config } = require('dotenv/types');
-
+var 
 const toneAnalyzer = new ToneAnalyzerV3({
   version: '2017-09-21',
   authenticator: new IamAuthenticator({
@@ -24,4 +12,32 @@ const toneAnalyzer = new ToneAnalyzerV3({
     'X-Watson-Learning-Opt-Out': 'true'
   }
 });
+
+exports.renderIndex = (req, res) => {
+    res.render("index");
+}
+
+exports.postData = (req, res) =>{
+    console.log(req.body)
+    const params = {
+        'tone_input' : {'text': text},
+        'content_type' : 'application/json'
+    };
+    toneAnalyzer.tone(params, function(error, response){
+        if(error){
+            console.log('error',error);
+        }else{
+            res.render ("index", {
+                delpost:JSON.stringify(response,NULL,2)
+            });
+        }
+
+    })
+
+
+  
+}
+//--------------------
+
+
 
