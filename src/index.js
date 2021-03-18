0e90cf8f4f6bb2c07592970257d9b17669bd0e9e
PedroGtz <pedr0.gtz@hotmail.com>
2021-03-17 20:55:19 -0600

    pues nada aun, empezando toneanalyzer

--- src/index.js
+++ src/index.js
@@ -8,3 +8,20 @@ exports.postData = (req, res) =>{
         delpost:req.body.fname
     });
 }
+//--------------------
+
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+const { config } = require('dotenv/types');
+
+const toneAnalyzer = new ToneAnalyzerV3({
+  version: '2017-09-21',
+  authenticator: new IamAuthenticator({
+    apikey: config.apikey,
+  }),
+  serviceUrl: config.serviceUrl,
+  headers: {
+    'X-Watson-Learning-Opt-Out': 'true'
+  }
+});
+
